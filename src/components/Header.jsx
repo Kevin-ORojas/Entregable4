944eec2799e3e5b7225084f3fe4dc3c0ed268514
NozoDev <kevinroj326@gmail.com>
2023-04-24 11:53:45 -0500

    Center PLus icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({setIsShowForm}) => {
     <header className='grid grid-cols-2  sm:p-8 md:p-4 mx-auto  bg-slate-500  '>
       <h1 className='text-2xl font-bold'>Usuarios</h1>
 
-      <button onClick={handleClickShowModal} className=" bg-purple-p col-end-4  text-white p-2 hover:bg-purple-p/90 cursor-pointer transition-colors text-sm"><i onClick={setIsShowForm} className=' bx bx-plus'></i>Crear nuevo usuario</button>
+      <button onClick={handleClickShowModal} className=" bg-purple-p col-end-4  text-white p-2 hover:bg-purple-p/90 cursor-pointer transition-colors text-sm  "><i onClick={setIsShowForm} className='text-center py-2 bx bx-plus'></i>Crear nuevo usuario</button>
       
     </header>
   )
